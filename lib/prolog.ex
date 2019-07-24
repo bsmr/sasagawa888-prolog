f4bab8d0bc2dc953805a91abdd07b60bfe502570
sasagawa888 <ken-s@tkcnf.or.jp>
2019-07-24 18:14:21 +0900

    Added test code

--- lib/prolog.ex
+++ lib/prolog.ex
@@ -428,21 +428,6 @@ defmodule Prove do
     end
   end
 
-  def test(x) do
-    env =  [[[:A, 5], 1],
-      [[:A1, 5], 1],
-  [[:N1, 5], 0],
-  [[:A1, 3], [:A, 5]],
-  [[:N, 5], 1],
-  [[:N1, 3], 1],
-  [[:A1, 1], [:A, 3]],
-  [[:N, 3], 2],
-  [[:N1, 1], 2],
-  [:X, [:A, 1]],
-  [[:N, 1], 3]]
-    deref(x,env)
-  end
-
   def deref1(_,[],_) do false end
   def deref1(x,[[x,v]|_],env) do
     if !is_var(v) do
--- test/prolog_test.exs
+++ test/prolog_test.exs
@@ -2,7 +2,10 @@ defmodule PrologTest do
   use ExUnit.Case
   doctest Prolog
 
-  test "greets the world" do
-    assert Prolog.hello() == :world
+  test "deref" do
+    env =  [[[:A, 5], 1],[[:A1, 5], 1],[[:N1, 5], 0],[[:A1, 3], [:A, 5]],
+            [[:N, 5], 1],[[:N1, 3], 1],[[:A1, 1], [:A, 3]],[[:N, 3], 2],
+            [[:N1, 1], 2],[:X, [:A, 1]],[[:N, 1], 3]]
+    assert Prove.deref([:A1,3],env) == 1
   end
 end
